f0421e57ce713aadef41485b1a471273fee3129a
Christian <christianmuller585@gmail.com>
2022-12-11 19:13:50 +0200

    update model

--- CarRacing/main.py
+++ CarRacing/main.py
@@ -1,4 +1,4 @@
-import os 
+import os
 import time
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
@@ -14,39 +14,51 @@ if not os.path.exists(model_dir):
     os.makedirs(model_dir)
 
 # hyper params
-num_envs = 1
-train = False
-learning_rate =  0.5
-num_steps = 2048
-batch_size = 32
+num_envs = 2
+train = True
+learning_rate = 7.830628567129431e-05
+num_steps = 32
 n_epochs = 20
-gamma = 0.99
+gamma = 0.98
+clip_range = 0.4
+batch_size = 16
+target_kl = 0.99
+gae_lambda = 0.9144383227774225
+
+
+if train == True:
+    num_envs = 2
+else:
+    num_envs = 1
 
 env = make_vec_env(model_name, num_envs)
 #env = gym.make(model_name)
+print(env.action_space)
 policy = 'MlpPolicy'
-total_timestamps = 50000
+total_timestamps = 100000
 
 if train:
-    env.reset()
     model = PPO(policy=policy,
                 env=env,
-                learning_rate = learning_rate,
-                n_steps = num_steps,
-                batch_size= batch_size,
-                n_epochs= n_epochs,
-                gamma= gamma,
+                learning_rate=learning_rate,
+                n_steps=num_steps,
+                n_epochs=n_epochs,
+                gamma=gamma,
+                clip_range=clip_range,
+                batch_size=batch_size,
+                target_kl= target_kl,
+                gae_lambda=gae_lambda,
                 tensorboard_log=log_dir,
                 verbose=1)
     model.learn(total_timesteps=total_timestamps)
-    err = model.save(f"{model_dir}/{model_name}")
-    print("model saved: " + err)
+    model.save(f"{model_dir}/{model_name}")
+    print("model saved")
 
 if not train:
     state = env.reset()
     model = PPO.load(f"{model_dir}/{model_name}")
     for i in range(1000):
-        action , _state = model.predict(state, deterministic= True)
+        action, _state = model.predict(state, deterministic=True)
         state, reward, done, info = env.step(action)
         time.sleep(0)
         env.render()
--- CarRacing/otimization.py
+++ CarRacing/otimization.py
@@ -9,14 +9,17 @@ from stable_baselines3.common.env_util import make_vec_env
 from typing import Any, Dict
 
 
-def objective(trial)-> Dict[str, Any]:
+def objective(trial) -> Dict[str, Any]:
     # Define the search space
-    learning_rate =  trial.suggest_loguniform("learning_rate", 1e-5, 1)
+    learning_rate = trial.suggest_loguniform("learning_rate", 1e-5, 1)
     n_steps = trial.suggest_categorical("n_steps", [8, 16, 32, 64, 128, 256, 512, 1024, 2048])
     n_epochs = trial.suggest_categorical("n_epochs", [1, 5, 10, 20])
     gamma = trial.suggest_categorical("gamma", [0.9, 0.95, 0.98, 0.99, 0.995, 0.999, 0.9999])
+    clip_range = trial.suggest_categorical("clip_range", [0.1, 0.2, 0.3, 0.4])
     batch_size = trial.suggest_categorical("batch_size", [8, 16, 32, 64, 128, 256, 512])
-    
+    tartget_kl = trial.suggest_categorical("tartget_kl", [0.9, 0.95, 0.98, 0.99, 0.995, 0.999, 0.9999])
+    gae_lambda = trial.suggest_loguniform("gae_lambda", 0.9, 1.0)
+
     model_name = "CarRacing-v0"
     model_dir = f"model/{model_name}"
     log_dir = "./logs"
@@ -29,23 +32,30 @@ def objective(trial)-> Dict[str, Any]:
                 env=env,
                 verbose=1,
                 learning_rate=learning_rate,
-                n_steps = n_steps,
-                n_epochs = n_epochs,
-                gamma = gamma)
+                n_steps=n_steps,
+                n_epochs=n_epochs,
+                gamma=gamma,
+                clip_range=clip_range,
+                batch_size=batch_size,
+                target_kl= tartget_kl,
+                gae_lambda=gae_lambda)
     model.learn(total_timesteps=total_timestamps)
 
     state = env.reset()
-    action , _state = model.predict(state)
-    state, reward, done, info = env.step(action)
+    total_reward = 0
+    for x in range(700):
+        action, _state = model.predict(state)
+        state, reward, done, info = env.step(action)
+        total_reward += reward
 
     env.close()
-    return reward
+    return total_reward
 
 
 study = optuna.create_study(study_name="CarRacing",
                             direction='maximize',
                             sampler=TPESampler())
-study.optimize(objective,show_progress_bar=True, n_trials=10)
+study.optimize(objective, show_progress_bar=True, n_trials=100)
 
 print("##############################################################")
 print(study.best_params)
